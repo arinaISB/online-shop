cdf6d2c1bf005e7067e7397686f46bc0404c825e
Arina Idam-Syuryun <idamsyuryunarina@gmail.om>
2024-02-05 18:16:40 +0300

    транзакции

--- app/Service/OrderService.php
+++ app/Service/OrderService.php
@@ -4,6 +4,7 @@ namespace Service;
 
 use Model\Cart;
 use Model\CartProduct;
+use Model\Model;
 use Model\OrderedCart;
 use Model\PlacedOrder;
 use Model\User;
@@ -13,17 +14,25 @@ class OrderService
 {
     public function create(PlacedOrder $order, User $user): void
     {
-        $order->save();
+        $pdo = Model::getPdo();
         $cart = Cart::getOneByUserId($user->getId());
         $cartProducts = CartProduct::getAllByCartId($cart->getId());
         $orderId = $order->getId();
 
-        foreach ($cartProducts as $productInCart)
-        {
-            $product = CartProductResource::format($productInCart);
-            OrderedCart::addOrderedItems($orderId, $product['id'], $productInCart->getQuantity(), $product['lineTotal']);
+        $pdo->beginTransaction();
+        try {
+            $order->save();
+            foreach ($cartProducts as $productInCart)
+            {
+                $product = CartProductResource::format($productInCart);
+                OrderedCart::addOrderedItems($orderId, $product['id'], $productInCart->getQuantity(), $product['lineTotal']);
+            }
+            CartProduct::deleteProductByCart($cart->getId());
+            $pdo->commit();
+
+        } catch (\Throwable $exception) {
+            $pdo->rollBack();
         }
-        CartProduct::deleteProductByCart($cart->getId());
     }
 
 }
